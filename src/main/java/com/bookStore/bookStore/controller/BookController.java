13e7c8ba8f407aae0349353daa9bc27306f53181
abidwal <bidansh007@gmail.com>
2023-10-15 19:18:44 +0530

    2 commit

--- src/main/java/com/bookStore/bookStore/controller/BookController.java
+++ src/main/java/com/bookStore/bookStore/controller/BookController.java
@@ -1,67 +1,61 @@
 package com.bookStore.bookStore.controller;
 
 import com.bookStore.bookStore.entities.Book;
-import com.bookStore.bookStore.entities.MyBookList;
-import com.bookStore.bookStore.service.BookService;
-import com.bookStore.bookStore.service.MyBookListService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
-@Controller
+@RestController("/api")
 public class BookController {
-  @Autowired
-  private BookService bookService;
-  @Autowired
-  private MyBookListService myBookListService;
-    @GetMapping("/")
-    public String home(){
-      return "Home";
+  @PersistenceContext
+  private EntityManager entityManager; // An API that manages the lifecycle of entity instance.
+  @GetMapping("/books")
+  public List<Book> getAllBooks(){
+    TypedQuery<Book> query = entityManager.createQuery("SELECT b FROM Book b", Book.class);
+    return query.getResultList();
+  }
+  @GetMapping("/books/{id}")
+  public Book getBookById(@PathVariable Long id){
+    return entityManager.find(Book.class,id);
+  }
+
+  @PostMapping
+  @Transactional
+  public Book createBook(@RequestBody Book book){
+    entityManager.persist(book);
+    return book;
+  }
+
+  @PutMapping("/{id}")
+  @Transactional
+  public ResponseEntity<Object> updateBookById(@PathVariable Long id, @RequestBody Book updatedBook){
+    Book exixtingBook = entityManager.find(Book.class,id);
+    if (exixtingBook != null){
+      exixtingBook.setId(updatedBook.getId());
+      exixtingBook.setName(updatedBook.getName());
+      exixtingBook.setAuthor(updatedBook.getAuthor());
+      exixtingBook.setPrice(updatedBook.getPrice());
+      return ResponseEntity.ok(exixtingBook);
+    }else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Book with ID " + id + " not found.");
     }
-   @GetMapping("/bookregister")
-   public String bookRegister(){
-      return "bookRegister";
-  }
-  @GetMapping("/availablebooks")
-  public ModelAndView getAllBooks(){
-    List<Book> list= bookService.getAllBook();
-//    ModelAndView m =new ModelAndView();
-//    m.setViewName("booklist");
-//    m.addObject("book",list);
-    return new ModelAndView("booklist","book",list);
-  }
-  @PostMapping("/save")
-      public String addBook(@ModelAttribute Book b){
-      bookService.save(b);
-      return "redirect:/availablebooks";
   }
 
-  @GetMapping("/mybooks")
-  public String getMyBooks(Model model){
-    List<MyBookList> list= myBookListService.getAllMyBooks();
-    model.addAttribute("book",list);
-      return "myBooks";
-  }
-  @RequestMapping("/mylist/{id}")
-  public String getMyList(@PathVariable("id")int id){
-      Book b= bookService.getBookById(id);
-      MyBookList mb=new MyBookList(b.getId(),b.getName(),b.getAuthor(),b.getPrice());
-      myBookListService.saveMyBooks(mb);
-      return "redirect:/mybooks";
-  }
-  @RequestMapping("/editBook/{id}")
-  public String editBook(@PathVariable("id")int id,Model model){
-      Book b = bookService.getBookById(id);
-      model.addAttribute("book",b);
-      return "bookEdit";
-  }
-
-  @RequestMapping("/deleteBook/{id}")
-  public String deleteBook(@PathVariable("id")int id){
-      bookService.deleteById(id);
-      return "redirect:/availablebooks";
-  }
+  @DeleteMapping("/{id}")
+  @Transactional
+    public ResponseEntity<Object> deleteBook(@PathVariable Long id){
+      Book book = entityManager.find(Book.class,id);
+      if (book != null){
+        entityManager.remove(book);
+        return ResponseEntity.ok("Book with ID " + id + " is deleted." );
+      }else {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Book with ID " + id + " not found.");
+      }
+    }
 }
+
