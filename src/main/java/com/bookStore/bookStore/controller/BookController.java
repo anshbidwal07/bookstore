1424e7125f48e6bdcfd964ee0a72822f2d1b3ae4
abidwal <bidansh007@gmail.com>
2023-10-17 12:27:30 +0530

    again commit

--- src/main/java/com/bookStore/bookStore/controller/BookController.java
+++ src/main/java/com/bookStore/bookStore/controller/BookController.java
@@ -1,61 +1,64 @@
 package com.bookStore.bookStore.controller;
 
 import com.bookStore.bookStore.entities.Book;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
-@RestController("/api")
+@RestController
+@RequestMapping("/api/books")
 public class BookController {
-  @PersistenceContext
-  private EntityManager entityManager; // An API that manages the lifecycle of entity instance.
-  @GetMapping("/books")
-  public List<Book> getAllBooks(){
-    TypedQuery<Book> query = entityManager.createQuery("SELECT b FROM Book b", Book.class);
-    return query.getResultList();
+
+  private List<Book> books = new ArrayList<>();
+
+  @GetMapping
+  public List<Book> getAllBooks() {
+    return books;
   }
-  @GetMapping("/books/{id}")
-  public Book getBookById(@PathVariable Long id){
-    return entityManager.find(Book.class,id);
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Object> getBookById(@PathVariable Long id) {
+    for (Book book : books) {
+      if (book.getId().equals(id)) {
+        return ResponseEntity.ok(book);
+      }
+    }
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Book with ID " + id + " not found.");
   }
 
   @PostMapping
-  @Transactional
-  public Book createBook(@RequestBody Book book){
-    entityManager.persist(book);
-    return book;
+  public ResponseEntity<Object> createBook(@RequestBody Book book) {
+    books.add(book);
+    return ResponseEntity.ok(book);
   }
 
   @PutMapping("/{id}")
-  @Transactional
-  public ResponseEntity<Object> updateBookById(@PathVariable Long id, @RequestBody Book updatedBook){
-    Book exixtingBook = entityManager.find(Book.class,id);
-    if (exixtingBook != null){
-      exixtingBook.setId(updatedBook.getId());
-      exixtingBook.setName(updatedBook.getName());
-      exixtingBook.setAuthor(updatedBook.getAuthor());
-      exixtingBook.setPrice(updatedBook.getPrice());
-      return ResponseEntity.ok(exixtingBook);
-    }else {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Book with ID " + id + " not found.");
+  public ResponseEntity<Object> updateBook(@PathVariable Long id, @RequestBody Book updatedBook) {
+    for (Book book : books) {
+      if (book.getId().equals(id)) {
+        book.setId(updatedBook.getId());
+        book.setPrice(updatedBook.getPrice());
+        book.setAuthor(updatedBook.getAuthor());
+        return ResponseEntity.ok(book);
+      }
     }
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Book with ID " + id + " not found.");
   }
 
   @DeleteMapping("/{id}")
-  @Transactional
-    public ResponseEntity<Object> deleteBook(@PathVariable Long id){
-      Book book = entityManager.find(Book.class,id);
-      if (book != null){
-        entityManager.remove(book);
-        return ResponseEntity.ok("Book with ID " + id + " is deleted." );
-      }else {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Book with ID " + id + " not found.");
+  public ResponseEntity<Object> deleteBook(@PathVariable Long id) {
+    for (Book book : books) {
+      if (book.getId().equals(id)) {
+        books.remove(book);
+        return ResponseEntity.ok("Book with ID " + id + " is deleted.");
       }
     }
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Book with ID " + id + " not found.");
+  }
+
 }
 
