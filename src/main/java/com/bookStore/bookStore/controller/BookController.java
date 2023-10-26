20a70a284b16d2cc1e2eada72ff42c6b0c5a74e1
abidwal <bidansh007@gmail.com>
2023-10-26 14:41:35 +0530

    calling service layer

--- src/main/java/com/bookStore/bookStore/controller/BookController.java
+++ src/main/java/com/bookStore/bookStore/controller/BookController.java
@@ -2,6 +2,7 @@ package com.bookStore.bookStore.controller;
 
 import com.bookStore.bookStore.entities.Book;
 import com.bookStore.bookStore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +12,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
+  @Autowired
+  private BookService bookService;
 
-  private final BookService bookService;
 
-  public BookController(BookService bookService) {
-    this.bookService = bookService;
-  }
+//  public BookController(BookService bookService) {
+//    this.bookService = bookService;
+//  }
 
   @GetMapping
   public List<Book> getAllBooks() {
@@ -24,32 +26,31 @@ public class BookController {
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<Object> getBookById(@PathVariable Long id) {
-    Book book = bookService.getBookById(id);
-    if (book != null) {
-      return ResponseEntity.ok(book);
-    } else {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Book with ID " + id + " not found.");
-    }
+  public Book getBookById(@PathVariable Long id) {
+   return bookService.getBookById(id);
   }
   @PostMapping("/{id}")
-  public ResponseEntity<Object> createBook(@RequestBody Book book) {
-   Book createdBook = bookService.createBook(book);
-    return ResponseEntity.ok(createdBook);
+  public String createBook(@RequestBody Book book) {
+    Book createBook = bookService.createBook(book);
+    if (createBook != null){
+      return "Book Created with ID: " + createBook.getId();
+    }else {
+      return "Falied to create book.";
+    }
   }
 
   @PutMapping("/{id}")
-  public ResponseEntity<Object> updateBook(@PathVariable Long id, @RequestBody Book updatedBook) {
+  public String updateBook(@PathVariable Long id, @RequestBody Book updatedBook) {
     Book book = bookService.updatedBook(id,updatedBook);
-    if(book != null){
-      return ResponseEntity.ok(book);
+    if(book!=null){
+      return "Book with ID " + id + " is updated.";
     }else {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Book with ID " + id + " not found.");
+      return "Book with ID " + id + " not found.";
     }
   }
 
   @DeleteMapping("/{id}")
-  public ResponseEntity<Object> deleteBook(@PathVariable Long id) {
+  public ResponseEntity<String> deleteBook(@PathVariable Long id) {
 
     if (bookService.deleteBook(id)) {
       return ResponseEntity.ok("Book with ID " + id + " is deleted.");
--- src/main/java/com/bookStore/bookStore/service/BookServiceImpl.java
+++ src/main/java/com/bookStore/bookStore/service/BookServiceImpl.java
@@ -8,12 +8,12 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
 public class BookServiceImpl implements BookService{
-  private final BookRepository bookRepository;
-
   @Autowired
-  public BookServiceImpl(BookRepository bookRepository) {
-    this.bookRepository = bookRepository;
-  }
+  private BookRepository bookRepository;
+
+//  public BookServiceImpl(BookRepository bookRepository) {
+//    this.bookRepository = bookRepository;
+//  }
 
   @Override
   public List<Book> getAllBooks() {
